ef411fcca302037e7504910540a4ceb7185be174
Danielle Smith <smith.ddms@gmail.com>
2023-06-30 05:15:49 -0500

    Additions to Analyze Module

--- TextToolKit/Analyze.cs
+++ TextToolKit/Analyze.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 namespace TextToolKit
 {
     public class Analyze
@@ -10,71 +11,173 @@ namespace TextToolKit
 
         public void CreateAnalysis(string text)
         {
+            //Character Data
+
+            //Char Counts
             int letterCount = text.Where(x => char.IsLetter(x)).Count();
             int numberCount = text.Where(x => char.IsNumber(x)).Count();
             int spaceCount = text.Where(x => x == ' ').Count();
             int otherCount = text.Where(x => !char.IsLetter(x) && !char.IsNumber(x) && x != ' ').Count();
             int totalCount = text.Length;
 
-            int wordCount = 0;
-            if (text.Contains(' ') && text.Any(x => x != ' '))
+            //Char split, group, count
+            var splitChar = text.ToLower().Select(x => x.ToString());
+            var statsChar = splitChar.GroupBy(x => x).Select(x => x.Key).Select(x => new { Character = x, CharCount = splitChar.Where(y => y == x).Count() });
+
+            //Get the Most Used Character
+            int maxCount = statsChar.Max(x => x.CharCount);
+            string mostChar = "";
+            if (statsChar.All(x => x.CharCount == maxCount))
             {
-                wordCount = text.Split(' ').Where(x => !x.Contains(' ')).Count();
+                mostChar = "All characters occur the same amount of times.";
+            }
+            else
+            {
+                var maxCharGroup = statsChar.Where(x => x.CharCount == maxCount).Select(x => x.Character).OrderBy(x => x);
+                mostChar = maxCharGroup.Count() > 1 ? string.Join(" ", maxCharGroup).Replace("  ", "Space ") : maxCharGroup.First().Replace(" ", "Space");
+                if (mostChar.Contains("Space"))
+                {
+                    if (mostChar == "Space")
+                    {
+                        int nextMaxCount = statsChar.Where(x => x.Character == " ").Max(x => x.CharCount);
+                        var nextMaxCharGroup = statsChar.Where(x => x.CharCount == nextMaxCount).Select(x => x.Character).OrderBy(x => x);
+                        mostChar += " followed by ";
+                        mostChar += nextMaxCharGroup.Count() > 1 ? string.Join(" ", nextMaxCharGroup) : nextMaxCharGroup.First();
+                    }
+                    else
+                    {
+                        mostChar = mostChar.Replace("Space", "").Trim();
+                        mostChar += " Space";
+                    }
+                }
+            }
+            //Get the Least Used Character
+            string leastChar = "";
+            if (mostChar == "All characters occur the same amount of times.")
+            {
+                leastChar = "All characters occur the same amount of times.";
+            }
+            else
+            {
+                int minCount = statsChar.Min(x => x.CharCount);
+                var minCharGroup = statsChar.Where(x => x.CharCount == minCount).Select(x => x.Character).OrderBy(x => x);
+                leastChar = minCharGroup.Count() > 1 ? string.Join(" ", minCharGroup).Replace("  ", "Space ") : minCharGroup.First().Replace(" ", "Space");
+                if (leastChar.Contains("Space"))
+                {
+                    if (leastChar == "Space")
+                    {
+                        int nextMinCount = statsChar.Where(x => x.Character == " ").Min(x => x.CharCount);
+                        var nextMinCharGroup = statsChar.Where(x => x.CharCount == nextMinCount).Select(x => x.Character).OrderBy(x => x);
+                        leastChar += " followed by ";
+                        leastChar += nextMinCharGroup.Count() > 1 ? string.Join(" ", nextMinCharGroup) : nextMinCharGroup.First();
+                    }
+                    else
+                    {
+                        leastChar = mostChar.Replace("Space", "").Trim();
+                        leastChar += " Space";
+                    }
+                }
             }
 
-            var statsChar = text.ToLower().Select(x => new { Character = x, CharCount = text.Where(y => y == x).Count() });
 
-            string mostChar = statsChar.OrderBy(x => x.CharCount).Select(x => x.Character).Last().ToString();
-            if (mostChar == " " && statsChar.Count() > 1)
+            //Word Data
+
+            List<string> splitWords = new List<string>();
+            if (text.Contains(' '))
             {
-                var second = statsChar.OrderBy(x => x.CharCount).Select(x => x.Character).ToArray();
-                string secMostChar = second[second.Length - 2].ToString();
-                mostChar = $"Space. followed by {secMostChar}";
-            }
-            string leastChar = statsChar.OrderBy(x => x.CharCount).Select(x => x.Character).First().ToString();
-            if (leastChar == " " && statsChar.Count() > 1)
-            {
-                var second = statsChar.OrderBy(x => x.CharCount).Select(x => x.Character).ToArray();
-                string secLeastChar = second[1].ToString();
-                mostChar = $"Space. followed by {secLeastChar}";
+                string replace = text.Replace('.', ' ').Replace('!', ' ').Replace('?', ' ').Replace(',', ' ').Replace(';', ' ').Replace(':', ' ');
+                if (replace.Any(x => x != ' '))
+                {
+                    splitWords = replace.ToLower().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).ToList();
+                }
             }
 
-            var splitWord = wordCount > 0 ? text.ToLower().Split(' ').Where(x => !x.Contains(' ')) : null;
-            var statsWord = splitWord != null && splitWord.Count() > 0 ? splitWord.Select(x => new { Word = x, WordCount = splitWord.Where(y => y == x).Count() }) : null;
+            int wordCount = splitWords.Count;
 
-            string mostWord = statsWord != null && statsWord.Count() > 0 ? statsWord.OrderBy(x => x.WordCount).Select(x => x.Word).Last() : "-------------------";
-            string leastWord = statsWord != null && statsWord.Count() > 0 ? statsWord.OrderBy(x => x.WordCount).Select(x => x.Word).First() : "-------------------";
+            var statsWord = wordCount > 0 ? splitWords.GroupBy(x => x).Select(x => x.Key).Select(x => new { Word = x, WordCount = splitWords.Where(y => y == x).Count() }) : null;
 
+            string mostWord = "";
+            string leastWord = "";
+
+            if (wordCount == 0)
+            {
+                mostWord = "Entry contains no words.";
+                leastWord = "Entry contains no words.";
+            }
+            else
+            {
+                int maxWordCount = statsWord.Max(x => x.WordCount);
+                if (statsWord.All(x => x.WordCount == maxWordCount))
+                {
+                    mostWord = "All words occur the same amount of times.";
+                }
+                else
+                {
+                    var maxWordGroup = statsWord.Where(x => x.WordCount == maxWordCount).Select(x => x.Word).OrderBy(x => x);
+                    mostWord = maxWordGroup.Count() > 1 ? string.Join(", ", maxWordGroup) : maxWordGroup.First();
+                }
+
+                if (mostWord == "All words occur the same amount of times.")
+                {
+                    leastWord = "All words occur the same amount of times.";
+                }
+                else
+                {
+                    int minWordCount = statsWord.Min(x => x.WordCount);
+                    var minWordGroup = statsWord.Where(x => x.WordCount == minWordCount).Select(x => x.Word).OrderBy(x => x);
+                    leastWord = minWordGroup.Count() > 1 ? string.Join(", ", minWordGroup) : minWordGroup.First();
+                }
+            }
+
+            //User View
 
             Console.Clear();
+            Console.WriteLine("Entered Text:");
+            Console.WriteLine("_____________");
             Console.WriteLine(text);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("________________________________________________________________________________________________");
+            Console.WriteLine("* If space is the Most Used Character, it will be followed by the next most used character(s).");
+            Console.WriteLine("* If space is the Least Used Character, it will be followed by the next least used character(s).");
+            Console.WriteLine("* Letter casing is ignored in this analysis. All will be counted as lower case.");
+            Console.WriteLine("* Words are counted as characters before a space or ending punctuation ( .!?,;: ).");
+            Console.WriteLine("________________________________________________________________________________________________");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("Single Character Data");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Letter Count", letterCount));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Number Count", numberCount));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Space Count", spaceCount));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Other Character Count", otherCount));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Total Character Count", totalCount));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
+            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Most Used Character(s)", mostChar));
+            Console.WriteLine("_________________________________________________________________________________");
+            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Least Used Character(s)", leastChar));
+            Console.WriteLine("_________________________________________________________________________________");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Word Data");
+            Console.WriteLine("_________________________________________________________________________________");
             Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Word Count", wordCount));
-            Console.WriteLine("_______________________________________________");
-            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Most Used Character", mostChar));
-            Console.WriteLine("_______________________________________________");
-            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Least Used Character", leastChar));
-            Console.WriteLine("_______________________________________________");
-            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Most Used Word", mostWord));
-            Console.WriteLine("_______________________________________________");
-            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Least Used Word", leastWord));
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("_________________________________________________________________________________");
+            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Most Used Word(s)", mostWord));
+            Console.WriteLine("_________________________________________________________________________________");
+            Console.WriteLine(String.Format("{0,-25} |    {1, -21}", "Least Used Word(s)", leastWord));
+            Console.WriteLine("_________________________________________________________________________________");
 
+            string charsDisplay = "abcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?";
 
-
-            //Make a note to user that all case is ignored
-            //Make note to user that words are based on spaces and ending punctuations in entry text
+            //Next, work on indivdual char display and count
+            //Then, work on word display and count
         }
     }
 }
